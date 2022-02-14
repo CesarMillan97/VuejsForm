245adf959b4b65c5997436d5b0535fe6899dac4b
Cesar Millan <cesar@Cesars-MacBook-Air.local>
2022-02-14 14:12:16 -0600

    works on safari but doesn't work on chrome

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,9 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
      <label>Email:</label>
      <input type="email" required  v-model="email">
      <label>Password:</label>
      <input type="password" required  v-model="password">
+     <div v-if="passwordError" class="error">
+        {{ passwordError }}
+     </div>
      <label>Role:</label>
      <select v-model="role">
         <option value="developer">developer</option>
@@ -11,13 +14,17 @@
         <option value="architect">architect</option>
      </select>
      <label>Skills: <span style="font-weight: light">(Press enter to add new skill)</span> </label>
-     <input type="text" v-model="tempSkills" @keyup="addSkill">
-     <div v-for="skill in skills" :key="skill" class="pill">
+     <input type="text" v-model="tempSkills" @keyup.ctrl="addSkill">
+     <div v-for="skill in skills" :key="skill" class="pill" @click="Remove(skill)">
         {{ skill }}
      </div>
      <div class="terms">
         <input type="checkbox" v-model="terms" required>
         <label>Accept terms and conditions</label>
+     </div>
+
+     <div class="submit">
+        <button>Create account</button>
      </div>
 
   </form>
@@ -39,17 +46,36 @@ export default {
          terms: false,
          tempSkills: '',
          skills: [],
+         passwordError: '',
       }
    },
    methods: {
       addSkill(e){
-         if (e.key === "Enter" && this.tempSkills) {
+         console.log(e);
+         if (e.key === ',' && this.tempSkills) {
+         console.log('ok');
             if (!this.skills.includes(this.tempSkills)) {
                this.skills.push(this.tempSkills)
             } else {
                alert('Skill already added')
             }
             this.tempSkills = '';   
+         }
+      },
+      Remove(skill){
+         this.skills = this.skills.filter((item) => {
+            return skill !== item
+         })
+      },
+      handleSubmit(){
+         this.passwordError = this.password.length > 5 ?
+         '' : 'The password must be at least 6 characters';
+         if (!this.passwordError) {
+            console.log('email: ' + this.email);
+            console.log('password: ' + this.password);
+            console.log('role: ' + this.role);
+            console.log('terms: ' + this.terms);
+            console.log('skills: ' + this.skills);
          }
       }
    }
@@ -89,5 +115,34 @@ input[type="checkbox"]{
    margin: 0 10px 0 0;
    position: relative;
    top: 2px;
+}
+.pill{
+   display: inline-block;
+   margin: 20px 10px 0 0;
+   padding: 6px 12px;
+   background: #eee;
+   border-radius: 20px;
+   font-size: 12px;
+   letter-spacing: 1px;
+   font-weight: bold;
+   color: #777;
+   cursor: pointer;
+}
+button{
+   background: #0b6dff;
+   border: 0;
+   padding: 10px 20px;
+   margin-top: 20px;
+   color: white;
+   border-radius: 20px;
+}
+.submit{
+   text-align: center;
+}
+.error{
+   color: #ff0062;
+   margin-top: 10px;
+   font-size: 0.8em;
+   font-weight: bold;
 }
 </style>
